f453c3445b0078307a02314b151f78c658ace005
Evan P. Troendle <e.troendle@qub.ac.uk>
2023-09-02 17:48:55 +0100

    Update ga.js

--- ga.js
+++ ga.js
@@ -1,17 +1,25 @@
 // Google Analytics Tracking
-(function(i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;
-    (i[r] =
-        i[r] ||
-        function() {
-            (i[r].q = i[r].q || []).push(arguments);
-        }),
-        (i[r].l = 1 * new Date());
-    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-    a.async = 1;
-    a.src = g;
-    m.parentNode.insertBefore(a, m);
-})(window, document, 'script', 'https://www.googletagmanager.com/gtag/js?id=G-77STZSMHK8', 'ga');
+(function () {
+    var gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-77STZSMHK8'; // Replace with your tracking ID
+    var firstScript = document.getElementsByTagName('script')[0];
+    firstScript.parentNode.insertBefore(gaScript, firstScript);
+})();
 
-ga('create', 'G-77STZSMHK8', 'auto');
-ga('send', 'pageview');
+//(function(i, s, o, g, r, a, m) {
+//    i['GoogleAnalyticsObject'] = r;
+//    (i[r] =
+//        i[r] ||
+//        function() {
+//            (i[r].q = i[r].q || []).push(arguments);
+//        }),
+//        (i[r].l = 1 * new Date());
+//    (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+//    a.async = 1;
+//    a.src = g;
+//    m.parentNode.insertBefore(a, m);
+//})(window, document, 'script', 'https://www.googletagmanager.com/gtag/js?id=G-77STZSMHK8', 'ga');
+
+//ga('create', 'G-77STZSMHK8', 'auto');
+//ga('send', 'pageview');
